4487cd506bd90699b17272167cb156284f5d7e4a
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-05-25 18:54:32 +0300

    :bug: Use /tmp to create db for testing

--- src/tests/config.py
+++ src/tests/config.py
@@ -1,1 +1,5 @@
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test_db.db"
+from pathlib import Path
+
+db_path = Path("/tmp/").joinpath("test_db.db")
+open(db_path, "a").close()
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_path}"
