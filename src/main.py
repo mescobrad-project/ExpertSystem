452ca195d88b9da580b4e8afb240d0dcecf48a5d
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-05-26 16:38:11 +0300

    :art: Improve main file's structure & add DB init

--- src/main.py
+++ src/main.py
@@ -1,16 +1,36 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.routers import workflow
-from src.config import CORS_ORIGINS, CORS_CREDENTIALS, CORS_METHODS, CORS_HEADERS
-
-main = FastAPI()
-
-main.add_middleware(
-    CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
-    allow_credentials=CORS_CREDENTIALS,
-    allow_methods=CORS_METHODS,
-    allow_headers=CORS_HEADERS,
+from src.config import (
+    CORS_ORIGINS,
+    CORS_CREDENTIALS,
+    CORS_METHODS,
+    CORS_HEADERS,
+    PROJECT_NAME,
 )
+from src.database import create_tables
 
-main.include_router(workflow.router)
+
+def include_middlewares(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=CORS_ORIGINS,
+        allow_credentials=CORS_CREDENTIALS,
+        allow_methods=CORS_METHODS,
+        allow_headers=CORS_HEADERS,
+    )
+
+
+def include_router(app):
+    app.include_router(workflow.router)
+
+
+def start_app():
+    app = FastAPI(title=PROJECT_NAME)
+    include_router(app)
+    include_middlewares(app)
+    create_tables()
+    return app
+
+
+main = start_app()
