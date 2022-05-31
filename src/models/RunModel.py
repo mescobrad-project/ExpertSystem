3cd6aafe1200a3ab73974daa98f26b52a6c3a366
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-05-31 11:56:31 +0300

    :seedling: Update python models to fit migrations

--- src/models/RunModel.py
+++ src/models/RunModel.py
@@ -1,15 +1,15 @@
-from sqlalchemy import Column, Integer, ForeignKey, JSON
+from sqlalchemy import Column, ForeignKey, JSON
 from sqlalchemy.orm import relationship
 
-from src.models._base import Base
+from src.models._base import Base, GUID
 
 
 class Run(Base):
     __tablename__ = "runs"
 
-    workflow_id = Column(Integer, ForeignKey("workflows.id"))
-    state = Column(JSON)
-    steps = Column(JSON)
-    queue = Column(JSON)
+    workflow_id = Column(GUID(), ForeignKey("workflows.id"), index=True)
+    state = Column(JSON, nullable=True)
+    steps = Column(JSON, nullable=True)
+    queue = Column(JSON, nullable=True)
 
     workflow = relationship("Workflow", back_populates="runs")
