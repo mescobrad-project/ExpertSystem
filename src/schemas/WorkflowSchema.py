fb6ce78140902d95670854f6ae1645caa08528c0
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-06-01 20:18:54 +0300

    :card_file_box: Update WF schema to match CRUD

--- src/schemas/WorkflowSchema.py
+++ src/schemas/WorkflowSchema.py
@@ -3,11 +3,39 @@ from uuid import UUID, uuid4
 from src.schemas._base import Base
 
 
-class Workflow(Base):
+# Shared properties
+class WorkflowBase(Base):
+    name: str
+    description: str | None = None
+    tasks: dict | None = None
+
+
+# Properties to receive on obj creation
+class WorkflowCreate(WorkflowBase):
+    pass
+
+
+# Properties to receive on obj update
+class WorkflowUpdate(WorkflowBase):
+    pass
+
+
+# Properties shared by models stored in DB
+class WorkflowInDBBase(WorkflowBase):
     id: UUID = Field(default_factory=uuid4)
     name: str
-    description: str
-    tasks: dict
+    description: str | None = None
+    tasks: dict | None = None
 
     class Config:
         orm_mode = True
+
+
+# Properties to return to client
+class Workflow(WorkflowInDBBase):
+    pass
+
+
+# Properties properties stored in DB
+class WorkflowInDB(WorkflowInDBBase):
+    pass
