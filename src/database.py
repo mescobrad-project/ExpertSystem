5cae2698ae6a48249b71511b267478bbdff47a17
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-06-01 20:21:05 +0300

    :bug: Fix db connection error

--- src/database.py
+++ src/database.py
@@ -3,9 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from src.config import SQLALCHEMY_DATABASE_URL
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
