b66b3d8368538b6bd407cce4a5ea27aada1b48cc
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-06-06 17:53:21 +0300

    :children_crossing: Add fns 2 handle list of dicts

--- src/tests/utils/_base.py
+++ src/tests/utils/_base.py
@@ -1,6 +1,7 @@
 import random
 import string
 from uuid import uuid4
+from random import randint
 
 
 def random_lower_string() -> str:
@@ -21,5 +22,28 @@ def random_dict_obj() -> dict:
     return obj
 
 
+def random_listofdict_obj() -> list:
+    li = []
+    for _ in range(1, randint(2, 5)):
+        obj = {}
+        obj[random_unique_string()] = random_lower_string()
+        li.append(obj)
+
+    return li
+
+
 def check_if_dicts_match(d1: dict, d2: dict) -> bool:
     return set(d1.keys()) == set(d2.keys())
+
+
+def check_if_listofdicts_match(l1: list, l2: list) -> bool:
+    length = len(l1) == len(l2)
+
+    if not length:
+        return False
+
+    for i in l1:
+        if i not in l2:
+            return False
+
+    return True
