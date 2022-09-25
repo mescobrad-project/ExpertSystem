fe03373416544142e37d8035c754c6f2e4323785
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-09-25 23:43:14 +0300

    :construction: WFC Parse raw XML file to JSON  - Create JSON of tasks based on raw XML data  - TODO: Find if tasks return different types (e.g. script, manual...)  - TODO: Utilize it on WF creation  - TODO: Find a way to indicate script files from BPMN script tasks

--- src/engine/config.py
+++ src/engine/config.py
@@ -7,6 +7,25 @@ PARALLEL_GATEWAY = getenv("WFE_PARALLEL_GATEWAY", "ParallelGateway")
 MANUAL_TASK = getenv("WFE_MANUAL_TASK", "ManualTask")
 SCRIPT_TASK = getenv("WFE_SCRIPT_TASK", "ScriptTask")
 
+XML_START_EVENT = "startEvent"
+XML_END_EVENT = "endEvent"
+XML_EXCLUSIVE_GATEWAY = "exclusiveGateway"
+XML_PARALLEL_GATEWAY = "parallelGateway"
+XML_MANUAL_TASK = "manualTask"
+XML_SCRIPT_TASK = "scriptTask"
+XML_FLOW = "sequenceFlow"
+XML_INCOMING = "incoming"
+XML_OUTGOING = "outgoing"
+XML_NAMESPACES = {
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+    "bpmn2": "http://www.omg.org/spec/BPMN/20100524/MODEL",
+    "bpmndi": "http://www.omg.org/spec/BPMN/20100524/DI",
+    "dc": "http://www.omg.org/spec/DD/20100524/DC",
+    "di": "http://www.omg.org/spec/DD/20100524/DI",
+}
+XML_PARSER_INPUTS = "inputs"
+XML_PARSER_OUTPUTS = "outputs"
+
 KEY_OUTPUT = getenv("WFE_KEY_OUTPUT", "outputs")
 
 SCRIPT_DIR = getenv("WFE_SCRIPT_DIR", "scripts")
