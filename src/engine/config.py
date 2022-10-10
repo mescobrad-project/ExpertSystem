8fd1949886926363e0687527c8909954bcafbf29
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-10-10 22:05:23 +0300

    🔧 Add user task and data related conf vars

--- src/engine/config.py
+++ src/engine/config.py
@@ -6,6 +6,9 @@ EXCLUSIVE_GATEWAY = getenv("WFE_EXCLUSIVE_GATEWAY", "ExclusiveGateway")
 PARALLEL_GATEWAY = getenv("WFE_PARALLEL_GATEWAY", "ParallelGateway")
 MANUAL_TASK = getenv("WFE_MANUAL_TASK", "ManualTask")
 SCRIPT_TASK = getenv("WFE_SCRIPT_TASK", "ScriptTask")
+USER_TASK = getenv("WFE_USER_TASK", "UserTask")
+DATA_STORE = getenv("WFE_DATA_STORE", "DataStore")
+DATA_OBJECT = getenv("WFE_DATA_OBJECT", "DataObject")
 
 XML_START_EVENT = "startEvent"
 XML_END_EVENT = "endEvent"
@@ -18,6 +21,12 @@ XML_INCOMING = "incoming"
 XML_OUTGOING = "outgoing"
 XML_ANNOTATION = "textAnnotation"
 XML_ASSOCIATION = "association"
+XML_USER_TASK = "userTask"
+XML_DATA_INPUT_ASSOCIATION = "dataInputAssociation"
+XML_DATA_OUTPUT_ASSOCIATION = "dataOutputAssociation"
+XML_DATA_STORE_REFERENCE = "dataStoreReference"
+XML_DATA_OBJECT_REFERENCE = "dataObjectReference"
+XML_DATA_OBJECT = "dataObject"
 XML_NAMESPACES = {
     "xsi": "http://www.w3.org/2001/XMLSchema-instance",
     "bpmn2": "http://www.omg.org/spec/BPMN/20100524/MODEL",
@@ -27,6 +36,8 @@ XML_NAMESPACES = {
 }
 XML_PARSER_INPUTS = "inputs"
 XML_PARSER_OUTPUTS = "outputs"
+XML_PARSER_DATA_INPUT_ASSOCIATIONS = "stores"
+XML_PARSER_DATA_OUTPUT_ASSOCIATIONS = "stores"
 
 KEY_OUTPUT = getenv("WFE_KEY_OUTPUT", "outputs")
 
