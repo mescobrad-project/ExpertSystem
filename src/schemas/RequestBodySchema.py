014e2f7f6698b1f8086b84b0829ffb33ec5c97f6
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-10-12 19:10:13 +0300

    👔 Update req b. schema 2 match complex store data

--- src/schemas/RequestBodySchema.py
+++ src/schemas/RequestBodySchema.py
@@ -1,6 +1,18 @@
+from uuid import UUID
 from pydantic import BaseModel
+
+
+class BpmnDataObject(BaseModel):
+    bucket_name: str
+    object_name: str
+
+
+class BpmnDataStore(BaseModel):
+    table: str
+    prop: str
+    id: str | UUID
 
 
 # Used to model the request body params of task complete route
 class TaskMetadataBodyParameter(BaseModel):
-    objectDataFilePaths: list = []
+    store: dict[str, dict[str, list[BpmnDataObject] | list[BpmnDataStore]]] = {}
