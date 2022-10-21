b495ce8a5b84ade38d9afee80665062e8208c420
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-10-21 17:38:12 +0300

    ♻️ Refactor external API class to implement base

--- src/clients/artificialintelligence.py
+++ src/clients/artificialintelligence.py
@@ -1,31 +1,56 @@
-from requests import Session
-from requests.adapters import HTTPAdapter
+from .classes.BaseApi import BaseApi, BaseRouter
 from src.config import AI_API_BASE_URL
 from uuid import UUID
 
 
-class Route:
-    def __init__(self, host: str) -> None:
-        self.host = host
+class Router(BaseRouter):
+    def healthcheck(self) -> str:
+        """
+        :return: {host}/healthcheck
+        """
+        return f"{self.host}/healthcheck"
 
     def algo(self) -> str:
+        """
+        :return: {host}/algo
+        """
         return f"{self.host}/algo"
 
+    def regression(self) -> str:
+        """
+        :return: {host}/algo/regression
+        """
+        return f"{self.algo()}/regression"
 
-class Api:
-    def __init__(self, host: str) -> None:
-        self.route = Route(host)
-        self.session = Session()
+    def regression_svn(self) -> str:
+        """
+        :return: {host}/algo/regression/svn
+        """
+        return f"{self.regression()}/svn"
 
-        self.session.mount(host, HTTPAdapter(max_retries=3))
+    def regression_svn_train(self) -> str:
+        """
+        :return: {host}/algo/regression/svn/train
+        """
+        return f"{self.regression_svn()}/train"
 
-    def post_algo(self, data: dict) -> dict:
-        response = self.session.post(self.route.algo(), json=data)
+    def check_if_route_is_available(self, route_name) -> bool:
+        """
+        :return: if route is available in specific list
+        """
+        return f"{self.algo()}/{route_name}" in [
+            self.regression_svn(),
+        ]
 
-        if not response.ok:
-            return {}
 
-        return response.json()
+class Api(BaseApi):
+    def get_healthcheck(self) -> dict:
+        return self._response_wrapper(self.session.get(self.route.healthcheck()))
+
+    def post_regression_svn_train(self, data: dict) -> dict:
+        return self._response_wrapper(
+            self.session.post(self.route.regression_svn_train(), json=data)
+        )
 
 
-client = Api(AI_API_BASE_URL)
+client = Api(Router, AI_API_BASE_URL)
