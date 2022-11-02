e80d863a193b7e803af325e2547b53dca4809682
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-11-02 15:08:02 +0200

    🏷️🗃️ Add Module models and their Categories  - Add Models  - Add Schemas  - Add Migrations

---
+++ src/schemas/ModuleCategorySchema.py
@@ -0,0 +1,39 @@
+from pydantic import Field
+from uuid import UUID, uuid4
+from src.schemas._base import Base, FormBase
+
+
+# Shared properties
+class ModuleCategoryBase(FormBase):
+    code: str
+    name: str
+
+
+# Properties to receive on obj creation
+class ModuleCategoryCreate(ModuleCategoryBase):
+    pass
+
+
+# Properties to receive on obj update
+class ModuleCategoryUpdate(ModuleCategoryBase):
+    pass
+
+
+# Properties shared by models stored in DB
+class ModuleCategoryInDBBase(Base):
+    id: UUID = Field(default_factory=lambda: uuid4().hex)
+    name: str
+    code: str
+
+    class Config:
+        orm_mode = True
+
+
+# Properties to return to client
+class ModuleCategory(ModuleCategoryInDBBase):
+    pass
+
+
+# Properties stored in DB
+class ModuleCategoryInDB(ModuleCategoryInDBBase):
+    pass
