4056a6338be5ff1175b1c4a87840e6b855031271
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-11-16 14:21:44 +0200

    🚚👔🏷️ Update Modules to use the new Ctrl class

--- src/routers/module.py
+++ src/routers/module.py
@@ -1,22 +1,14 @@
 from typing import Any
 from uuid import UUID
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
-from datetime import datetime, timezone
 from src.database import get_db
-from src.models.ModuleCategoryModel import ModuleCategoryModel
-from src.models.ModuleModel import ModuleModel
 from src.controllers.ModuleController import ModuleController
 from src.schemas.ModuleSchema import (
     Module,
     ModuleCreate,
     ModuleUpdate,
 )
-from src.utils.pagination import append_query_in_uri
-from ._base import RouteHelper
-
-route_helper = RouteHelper(ModuleController)
 
 router = APIRouter(
     prefix="/module",
@@ -41,37 +33,9 @@ def read_modules(
     order: The model's prop as str, e.g. id
     direction: asc | desc
     """
-    criteria = {}
-    if category:
-        criteria["category"] = {
-            "model": ModuleCategoryModel,
-            "criteria": {"code": category},
-        }
-
-    if task:
-        criteria["task"] = task
-
-    criteria["deleted_at"] = None
-
-    response = route_helper.read_multi(db, skip, limit, order, direction, criteria)
-
-    if category != None:
-        response["paging"]["previous_link"] = append_query_in_uri(
-            response["paging"]["previous_link"], f"category={category}"
-        )
-        response["paging"]["next_link"] = append_query_in_uri(
-            response["paging"]["next_link"], f"category={category}"
-        )
-
-    if task != None:
-        response["paging"]["previous_link"] = append_query_in_uri(
-            response["paging"]["previous_link"], f"task={task}"
-        )
-        response["paging"]["next_link"] = append_query_in_uri(
-            response["paging"]["next_link"], f"task={task}"
-        )
-
-    return response
+    return ModuleController.read_multi(
+        db, skip, limit, order, direction, task, category, criteria={"deleted_at": None}
+    )
 
 
 @router.post("")
@@ -79,14 +43,7 @@ def create_module(*, db: Session = Depends(get_db), module_in: ModuleCreate) -> 
     """
     Create new module.
     """
-    try:
-        module = ModuleController.create(db=db, obj_in=module_in)
-    except IntegrityError:
-        raise HTTPException(status_code=409, detail="Module already exists")
-    except Exception:
-        raise HTTPException(status_code=400, detail="Provided input is wrong")
-
-    return module
+    return ModuleController.create(db=db, obj_in=module_in)
 
 
 @router.get("/deleted", response_model=dict[str, Any | list[Module]])
@@ -105,37 +62,16 @@ def read_modules(
     order: The model's prop as str, e.g. id
     direction: asc | desc
     """
-    criteria = {}
-    if category:
-        criteria["category"] = {
-            "model": ModuleCategoryModel,
-            "criteria": {"code": category},
-        }
-
-    if task:
-        criteria["task"] = task
-
-    criteria["deleted_at__not"] = None
-
-    response = route_helper.read_multi(db, skip, limit, order, direction, criteria)
-
-    if category != None:
-        response["paging"]["previous_link"] = append_query_in_uri(
-            response["paging"]["previous_link"], f"category={category}"
-        )
-        response["paging"]["next_link"] = append_query_in_uri(
-            response["paging"]["next_link"], f"category={category}"
-        )
-
-    if task != None:
-        response["paging"]["previous_link"] = append_query_in_uri(
-            response["paging"]["previous_link"], f"task={task}"
-        )
-        response["paging"]["next_link"] = append_query_in_uri(
-            response["paging"]["next_link"], f"task={task}"
-        )
-
-    return response
+    return ModuleController.read_multi(
+        db,
+        skip,
+        limit,
+        order,
+        direction,
+        task,
+        category,
+        criteria={"deleted_at__not": None},
+    )
 
 
 @router.get("/deleted/{module_id}", response_model=Module)
@@ -147,7 +83,9 @@ def read_deleted_module(
     """
     Get deleted module by ID.
     """
-    return route_helper.read(module_id, db=db, criteria={"deleted_at__not": None})
+    return ModuleController.read(
+        db=db, resource_id=module_id, criteria={"deleted_at__not": None}
+    )
 
 
 @router.get("/{module_id}", response_model=Module)
@@ -159,7 +97,9 @@ def read_module(
     """
     Get module by ID.
     """
-    return route_helper.read(module_id, db=db, criteria={"deleted_at": None})
+    return ModuleController.read(
+        db=db, resource_id=module_id, criteria={"deleted_at": None}
+    )
 
 
 @router.put("/{module_id}", response_model=Module)
@@ -172,9 +112,7 @@ def update_module(
     """
     Update a module.
     """
-    module = route_helper.read(module_id, db=db, criteria={"deleted_at": None})
-
-    return ModuleController.update(db=db, db_obj=module, obj_in=module_in)
+    return ModuleController.update(db=db, resource_id=module_id, resource_in=module_in)
 
 
 @router.delete("/{module_id}", response_model=Module)
@@ -186,17 +124,9 @@ def destroy_module(
     """
     Delete a module.
     """
-    module = route_helper.read(module_id, db=db, criteria={"deleted_at": None})
-
-    if module.deleted_at:
-        raise HTTPException(status_code=405, detail="Module already destroyed")
-
-    module_in = ModuleUpdate(
-        name=module.name, instructions=module.instructions, task=module.task
+    return ModuleController.destroy(
+        db=db, resource_id=module_id, resource_in=ModuleUpdate()
     )
-    module_in.deleted_at = datetime.now(tz=timezone.utc)
-    module = ModuleController.update(db=db, db_obj=module, obj_in=module_in)
-    return module
 
 
 @router.delete("/{module_id}/revert", response_model=Module)
@@ -208,14 +138,6 @@ def revert_module(
     """
     Revert the deletion of a module.
     """
-    module = route_helper.read(module_id, db=db, criteria={"deleted_at__not": None})
-
-    if not module.deleted_at:
-        raise HTTPException(status_code=405, detail="Module is active")
-
-    module_in = ModuleUpdate(
-        name=module.name, instructions=module.instructions, task=module.task
+    return ModuleController.revert(
+        db=db, resource_id=module_id, resource_in=ModuleUpdate()
     )
-    module_in.deleted_at = None
-    module = ModuleController.update(db=db, db_obj=module, obj_in=module_in)
-    return module
