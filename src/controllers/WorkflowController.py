ff2ec374a0d1394d192542097f7502e5e83bffa4
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2022-11-17 16:26:03 +0200

    🚚👔🏷️ Update WFs to use the new Ctrl classes

--- src/controllers/WorkflowController.py
+++ src/controllers/WorkflowController.py
@@ -1,152 +1,96 @@
-import xml.etree.ElementTree as ET
-from ._base import CRUDBase
-from src.models._all import WorkflowModel
+from uuid import UUID
+from sqlalchemy.orm import Session
+from src.errors.ApiRequestException import NotFoundException
+from src.models.RunModel import RunModel
+from src.models.WorkflowModel import WorkflowModel
+from src.models.WorkflowCategoryModel import WorkflowCategoryModel
+from src.repositories.WorkflowRepository import WorkflowRepository
 from src.schemas.WorkflowSchema import WorkflowCreate, WorkflowUpdate
-from src.engine.config import (
-    RECEIVE_TASK,
-    SEND_TASK,
-    START_EVENT,
-    END_EVENT,
-    EXCLUSIVE_GATEWAY,
-    PARALLEL_GATEWAY,
-    MANUAL_TASK,
-    SCRIPT_TASK,
-    USER_TASK,
-    DATA_STORE,
-    DATA_OBJECT,
-    XML_EXCLUSIVE_GATEWAY,
-    XML_MANUAL_TASK,
-    XML_RECEIVE_TASK,
-    XML_SCRIPT_TASK,
-    XML_FLOW,
-    XML_NAMESPACES,
-    XML_PARSER_INPUTS,
-    XML_PARSER_OUTPUTS,
-    XML_ANNOTATION,
-    XML_ASSOCIATION,
-    XML_SEND_TASK,
-    XML_USER_TASK,
-    XML_DATA_INPUT_ASSOCIATION,
-    XML_DATA_STORE_REFERENCE,
-    XML_DATA_OBJECT_REFERENCE,
-    XML_DATA_OBJECT,
-    XML_PARSER_DATA_INPUT_ASSOCIATIONS,
-    XML_PARSER_DATA_OUTPUT_ASSOCIATIONS,
-)
-from src.engine.utils.XmlUtils import (
-    get_task_type,
-    get_property_type,
-    parse_flows,
-    parse_annotations,
-    parse_data_object_refs,
-    parse_data_store_refs,
-    parse_associations,
-    parse_data_input_associations,
-)
+from src.utils.pagination import append_query_in_uri
+from src.utils.workflow import get_workflow_entity_types, parse_xml
+from ._base import BaseController
 
 
-class CRUDWorkflow(CRUDBase[WorkflowModel, WorkflowCreate, WorkflowUpdate]):
-    def get_workflow_entity_types(self):
-        return {
-            "event": [START_EVENT, END_EVENT],
-            "gateway": [EXCLUSIVE_GATEWAY, PARALLEL_GATEWAY],
-            "task": [MANUAL_TASK, SCRIPT_TASK, USER_TASK, SEND_TASK, RECEIVE_TASK],
-            "store": [DATA_STORE, DATA_OBJECT],
-        }
+class _WorkflowController(BaseController):
+    def read_multi(
+        self,
+        db: Session,
+        skip: int = 0,
+        limit: int = 100,
+        order: str = None,
+        direction: str = None,
+        category: str = None,
+        is_template: bool = False,
+        criteria: dict = {},
+    ):
+        if category:
+            criteria["category"] = {
+                "model": WorkflowCategoryModel,
+                "criteria": {"code": category},
+            }
 
-    def parse_xml(self, xml_str):
-        xml_decoded = ET.fromstring(xml_str)
+        criteria["is_template"] = is_template
 
-        processes = xml_decoded.findall("bpmn2:process", XML_NAMESPACES)
-        # print(xml_decoded.text, xml_decoded.tag, xml_decoded.attrib)
+        response = super().read_multi(db, skip, limit, order, direction, criteria)
 
-        sequence_flows = {}
-        text_annotations = {}
-        associations = {}
-        tasks = {}
-        stores = {}
-
-        for process in processes:
-            sequence_flows = parse_flows(
-                process.findall(f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_FLOW}')
+        if category != None:
+            response["paging"]["previous_link"] = append_query_in_uri(
+                response["paging"]["previous_link"], f"category={category}"
+            )
+            response["paging"]["next_link"] = append_query_in_uri(
+                response["paging"]["next_link"], f"category={category}"
             )
 
-            text_annotations = parse_annotations(
-                process.findall(f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_ANNOTATION}')
+        if is_template:
+            response["paging"]["previous_link"] = append_query_in_uri(
+                response["paging"]["previous_link"], f"is_template={is_template}"
+            )
+            response["paging"]["next_link"] = append_query_in_uri(
+                response["paging"]["next_link"], f"is_template={is_template}"
             )
 
-            associations = parse_associations(
-                process.findall(f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_ASSOCIATION}')
+        return response
+
+    def read_entity_types(self):
+        return get_workflow_entity_types()
+
+    def create(self, db: Session, *, obj_in: WorkflowCreate):
+        workflow = super().create(db, obj_in=obj_in)
+
+        [tasks, stores] = parse_xml(workflow.raw_diagram_data["xml_original"])
+        workflow_upd = WorkflowUpdate()
+        workflow_upd.tasks = tasks
+        workflow_upd.stores = stores
+
+        return super().update(db=db, resource_id=workflow.id, resource_in=workflow_upd)
+
+    def update(self, db: Session, resource_id: UUID, resource_in: WorkflowUpdate):
+        if resource_in.raw_diagram_data and resource_in.raw_diagram_data.get(
+            "xml_original"
+        ):
+            raw_diagram = resource_in.raw_diagram_data.get("xml_original")
+        else:
+            workflow = super().read(
+                db=db, resource_id=resource_id, criteria={"deleted_at": None}
             )
+            raw_diagram = workflow.raw_diagram_data["xml_original"]
 
-            stores.update(
-                parse_data_store_refs(
-                    process.findall(
-                        f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_DATA_STORE_REFERENCE}'
-                    )
-                )
-            )
-            stores.update(
-                parse_data_object_refs(
-                    process.findall(
-                        f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_DATA_OBJECT_REFERENCE}'
-                    )
-                )
-            )
+        [tasks, stores] = parse_xml(raw_diagram)
+        resource_in.tasks = tasks
+        resource_in.stores = stores
 
-            for child in process:
-                if child.tag in [
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_FLOW}',
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_ANNOTATION}',
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_ASSOCIATION}',
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_DATA_INPUT_ASSOCIATION}',
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_DATA_STORE_REFERENCE}',
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_DATA_OBJECT_REFERENCE}',
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_DATA_OBJECT}',
-                ]:
-                    continue
+        return super().update(db=db, resource_id=resource_id, resource_in=resource_in)
 
-                # print(child.text, child.tag, child.attrib)
-                task_id = child.attrib["id"]
-                tasks[task_id] = {
-                    "type": get_task_type(child.tag),
-                    "name": child.attrib.get("name") or task_id,
-                }
+    def read_task_details(self, db: Session, resource_id: UUID, task_name: str):
+        workflow = super().read(
+            db=db, resource_id=resource_id, criteria={"deleted_at": None}
+        )
 
-                tasks[task_id]["manual"] = child.tag in [
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_EXCLUSIVE_GATEWAY}',
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_MANUAL_TASK}',
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_USER_TASK}',
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_SEND_TASK}',
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_RECEIVE_TASK}',
-                ]
+        for key, task in workflow.tasks.items():
+            if key == task_name:
+                return task
 
-                for prop in child:
-                    # print(prop.text, prop.tag, prop.attrib)
-                    mode = get_property_type(prop.tag)
-                    if not mode:
-                        continue
-
-                    if mode not in tasks[task_id].keys():
-                        tasks[task_id][mode] = []
-
-                    if mode in [XML_PARSER_INPUTS, XML_PARSER_OUTPUTS]:
-                        tasks[task_id][mode].append(sequence_flows[prop.text][mode])
-                    elif mode in [
-                        XML_PARSER_DATA_INPUT_ASSOCIATIONS,
-                        XML_PARSER_DATA_OUTPUT_ASSOCIATIONS,
-                    ]:
-                        tasks[task_id][mode].append(
-                            parse_data_input_associations(stores, prop)
-                        )
-
-                if child.tag in [
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_SCRIPT_TASK}',
-                    f'{{{XML_NAMESPACES["bpmn2"]}}}{XML_SEND_TASK}',
-                ]:
-                    tasks[task_id]["class"] = text_annotations[associations[task_id]]
-
-        return [tasks, stores]
+        raise NotFoundException(details="Task not found in workflow")
 
 
-WorkflowController = CRUDWorkflow(WorkflowModel)
+WorkflowController = _WorkflowController(WorkflowRepository)
