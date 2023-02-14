3f101e6724c5daa0f5bc5ec020b24d9fcdb996f0
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2023-02-14 23:39:03 +0200

    🐛 Fix Feature & Variable schemas

--- src/schemas/_association.py
+++ src/schemas/_association.py
@@ -6,35 +6,48 @@ from src.schemas._base import Base, FormBase
 class FeatureBase(FormBase):
     name: str | None = None
     mapi_id: str | None = None
-    info: str | None = {}
-    variables: list["VariableBase"] | None = []
+    info: dict | None = None
+    variables: list["VariableBase"] | list | None = None
 
 
 class VariableBase(FormBase):
     name: str | None = None
     mapi_id: str | None = None
-    info: str | None = {}
-    features: list["FeatureBase"] | None = []
+    info: dict | None = None
+    features: list["FeatureBase"] | list | None = None
 
 
 # Properties shared by models stored in DB
-class FeatureInDBBase(Base):
+class FeatureInDBBaseWithoutRelationship(Base):
     id: UUID = Field(default_factory=lambda: uuid4().hex)
     name: str | None = None
     mapi_id: str | None = None
-    info: str | None = {}
-    variables: list["VariableInDBBase"] | None = []
+    info: dict | None = None
+
+
+# Properties shared by models stored in DB
+class FeatureInDBBase(FeatureInDBBaseWithoutRelationship):
+    variables: list["VariableInDBBaseWithoutRelationship"] | list | None = None
 
     class Config:
         orm_mode = True
 
 
-class VariableInDBBase(Base):
+class VariableInDBBaseWithoutRelationship(Base):
     id: UUID = Field(default_factory=lambda: uuid4().hex)
     name: str | None = None
     mapi_id: str | None = None
-    info: str | None = {}
-    features: list["FeatureInDBBase"] | None = []
+    info: dict | None = None
+
+
+class VariableInDBBase(VariableInDBBaseWithoutRelationship):
+    features: list["FeatureInDBBaseWithoutRelationship"] | list | None = None
 
     class Config:
         orm_mode = True
+
+
+FeatureInDBBase.update_forward_refs()
+VariableInDBBase.update_forward_refs()
+FeatureBase.update_forward_refs()
+VariableBase.update_forward_refs()
