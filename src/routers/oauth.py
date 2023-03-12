b9419d3712b54f86fd6944671635eb6102ea2b3f
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2023-03-12 19:16:30 +0200

    🚚 Move oauth callback to oauth route

--- src/routers/oauth.py
+++ src/routers/oauth.py
@@ -1,9 +1,9 @@
 from typing import Any
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from fastapi.security import OAuth2AuthorizationCodeBearer
 from sqlalchemy.orm import Session
 from src.controllers.OAuthController import OAuthController
-from src.config import OAUTH_HOST, OAUTH_TOKEN_URL
+from src.config import OAUTH_HOST, OAUTH_TOKEN_URL, ES_UI_BASE_URL
 from src.database import get_db
 from src.dependencies.authentication import clean_before_login, validate_user
 
@@ -24,6 +24,22 @@ def get_auth() -> Any:
     Get OAuth endpoint to redirect to log in.
     """
     return {"url": OAuthController.login()}
+
+
+@router.get("/callback")
+def oauth_callback(
+    *,
+    db: Session = Depends(get_db),
+    code: str,
+    clean_token=Depends(clean_before_login),
+) -> Any:
+    """
+    Route used to retrieve auth token.
+    """
+    token, user = OAuthController.oauth_callback(db, code)
+    return Response(
+        f"<html><body><script defer>window.location.assign('{ES_UI_BASE_URL}/auth/callback/{user}/{token}')</script></body></html>"
+    )
 
 
 @router.get("/logout")
