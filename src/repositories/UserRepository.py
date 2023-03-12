882f5fa2e4d18ed068c85c247ff244e022814c3c
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2023-03-12 17:35:45 +0200

    🐛 Fix User schemas

--- src/repositories/UserRepository.py
+++ src/repositories/UserRepository.py
@@ -1,9 +1,9 @@
 from src.models._all import UserModel
-from src.schemas.ModuleCategorySchema import ModuleCategoryCreate, ModuleCategoryUpdate
+from src.schemas.UserSchema import UserCreate, UserUpdate
 from ._base import BaseCRUD, Session, ModelType, _parse_criteria
 
 
-class _UserRepository(BaseCRUD[UserModel, ModuleCategoryCreate, ModuleCategoryUpdate]):
+class _UserRepository(BaseCRUD[UserModel, UserCreate, UserUpdate]):
     def get_from_sub_and_provider(
         self, db: Session, *, sub: str, provider: str
     ) -> ModelType:
