03d72570a751f6a8836b7f8c31b7eeb5b20485ce
Vassilis Stamoulos <b.stamoulos@yahoo.gr>
2023-12-20 00:43:57 +0200

    🥅 If source not found, return empty array

--- src/controllers/DataStorageController.py
+++ src/controllers/DataStorageController.py
@@ -1,4 +1,3 @@
-from sqlalchemy.sql.expression import text
 from trino.dbapi import Connection
 from src.clients.trino import client
 
@@ -35,13 +34,20 @@ class BaseController:
         }
 
     def list_catalog_table_sources(self, catalog: str, schema: str, table: str):
+        data = []
+
+        try:
+            data = self.__exec(
+                f"SELECT DISTINCT source FROM {catalog}.{schema}.{table}"
+            )
+        except:
+            data = []
+
         return {
             "catalog": catalog,
             "schema": schema,
             "table": table,
-            "data": self.__exec(
-                f"SELECT DISTINCT source FROM {catalog}.{schema}.{table}"
-            ),
+            "data": data,
         }
 
 
