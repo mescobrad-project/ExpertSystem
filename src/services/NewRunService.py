3a9e89914af85edd64aebb7a3eadddff48aaba5b
Evangelos Kolaitis <ekolaitis@ikapps.gr>
2024-05-24 16:27:29 +0300

    notes for data storage

--- src/services/NewRunService.py
+++ src/services/NewRunService.py
@@ -44,9 +44,9 @@ def execute_sql_on_trino(sql: str, schema: str, catalog: str, x_es_token: str = 
             verify=False,
         )
     cursor = client.cursor()
-    cursor.execute(f"USE {schema}")
-    cursor.execute(f"USE {catalog}")
-    cursor.execute(sql)
+    cursor.execute(f"USE iceberg")
+    cursor.execute(f"USE {schema}") # name of the bucket // Initial is the name of the organization
+    cursor.execute(sql) # catalog.schema.table, no schemas in schemas tabls is a folder structure inside schema
     return cursor.fetchall()
 
 def get_buckets_from_minio(bucket_name: str, x_es_token: str = Header()) -> Any:
@@ -74,4 +74,56 @@ def get_buckets_from_minio(bucket_name: str, x_es_token: str = Header()) -> Any:
     )
     buckets = client.list_buckets(bucket_name)
     return buckets
+
+def get_files_from_minio(bucket_name: str, x_es_token: str = Header()) -> Any:
+    minio_url = S3_ENDPOINT
+    minio_data = {
+        "Action": "AssumeRoleWithWebIdentity",
+        "Version": "2011-06-15",
+        "WebIdentityToken": x_es_token
+    }
+
+    response = requests.post(minio_url, data=minio_data)
+    xml_data = ElementTree.fromstring(response.text)
+
+    # Step 2: Parse the output to extract the credentials
+    access_key = xml_data.find('.//{https://sts.amazonaws.com/doc/2011-06-15/}AccessKeyId').text
+    secret_access_key = xml_data.find('.//{https://sts.amazonaws.com/doc/2011-06-15/}SecretAccessKey').text
+    session_token = xml_data.find('.//{https://sts.amazonaws.com/doc/2011-06-15/}SessionToken').text
+    
+    client = Minio(
+        endpoint=S3_ENDPOINT,
+        access_key=access_key,
+        secret_key=secret_access_key,
+        session_token=session_token,
+        secure=False,
+    )
+    files = client.list_objects(bucket_name)
+    return files
             
+def get_file_from_minio(bucket_name: str, file_name: str, x_es_token: str = Header()) -> Any:
+    minio_url = S3_ENDPOINT
+    minio_data = {
+        "Action": "AssumeRoleWithWebIdentity",
+        "Version": "2011-06-15",
+        "WebIdentityToken": x_es_token
+    }
+
+    response = requests.post(minio_url, data=minio_data)
+    xml_data = ElementTree.fromstring(response.text)
+
+    # Step 2: Parse the output to extract the credentials
+    access_key = xml_data.find('.//{https://sts.amazonaws.com/doc/2011-06-15/}AccessKeyId').text
+    secret_access_key = xml_data.find('.//{https://sts.amazonaws.com/doc/2011-06-15/}SecretAccessKey').text
+    session_token = xml_data.find('.//{https://sts.amazonaws.com/doc/2011-06-15/}SessionToken').text
+    
+    client = Minio(
+        endpoint=S3_ENDPOINT,
+        access_key=access_key,
+        secret_key=secret_access_key,
+        session_token=session_token,
+        secure=False,
+    )
+    file = client.get_object(bucket_name, file_name)
+    
+    return file
