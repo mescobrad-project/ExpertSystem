e058f608d4dcd62e90593b1dcdffdfc3e268d7fe
Evangelos Kolaitis <ekolaitis@ikapps.gr>
2024-06-17 18:10:08 +0300

    xfb

--- src/services/NewRunService.py
+++ src/services/NewRunService.py
@@ -461,6 +461,14 @@ def complete_run(db: Session, run_id: UUID) -> Any:
     db.commit()
     return run
 
+
 def get_workflow_variable_names(db: Session, workflow_id: UUID) -> Any:
-    res = db.execute("SELECT DISTINCT(variable) as variable FROM new_workflow_action_conditionals WHERE workflow_action_id IN (SELECT id FROM new_workflow_actions WHERE workflow_id = :workflow_id)", {"workflow_id": str(workflow_id)}).fetchall()._mapping
+    res = (
+        db.execute(
+            "SELECT DISTINCT(variable) as variable FROM new_workflow_action_conditionals WHERE workflow_action_id IN (SELECT id FROM new_workflow_actions WHERE workflow_id = :workflow_id)",
+            {"workflow_id": str(workflow_id)},
+        )
+        .fetchall()
+        ._mapping
+    )
     return res
